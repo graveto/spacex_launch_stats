634b7d54af62133eaf130c956a9f21ebb6400072
David Fuka <gravetotkd@gmail.com>
2019-07-28 19:31:57 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+express = require('express');
+graphqlHTTP = require('express-graphql');
+const schema = require('./schema');
+
+const app = express();
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true
+}));
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
